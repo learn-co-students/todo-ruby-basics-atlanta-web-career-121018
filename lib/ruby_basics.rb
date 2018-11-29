b14092a1550c2d59dec6d9c3f059cfbce58f99e2
Vanessa Prema <vanessabprema@gmail.com>
2018-11-29 13:12:37 -0500

    Ruby Basics Lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+    quot = num1/num2
+    quot
 end
 
 def assign_variable(value)
-  
+    name = value
+    value
 end
 
-def argue
-  
+def argue(argue)
+    argue
 end
 
-def greeting
-
+def greeting(greeting,name)
 end
 
 def return_a_value
-
+    "Nice"
 end
 
 def last_evaluated_value
-
+    "expert"
 end
 
-def pizza_party
-
+def pizza_party(type="cheese")
+    type
 end
